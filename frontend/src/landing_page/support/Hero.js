59478b9c69a2277a70bb053b8af9528d0c340c31
Rahul <rd2432007@gmail.com>
2026-03-23 00:14:56 +0530

    commit

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,13 +1,32 @@
 import React from 'react';
 function Hero() {
     return ( 
-        <div className="container">
-      <div className="row p-5 mt-5 mb-5">
-        <h3>Support Portal</h3>
+      <section className="container-fluid" id="supportHero">
+      <div className="p-5" id="supportWrapper">
+        <h4>Support Portal</h4>
         <a href="">Track Tickets</a>
-        
       </div>
-    </div>
+
+      <div className="row p-5 m-5">
+        <div className="col-6 p-5">
+          <h1 className='fs-3'>Search for an answer or browser help topics to create a ticket</h1>
+          <input placeholder='Eg. how do I activate F&O '/><br/>
+          <a href="">Track account opening</a>
+          <a href="">Track segment activation</a>
+          <a href="">Intraday margins</a>
+          <a href="">Kite user manual</a>
+        </div>
+        <div className="col-6 p-5">
+          <h1 className='fs-3'>Featured</h1>
+          <ol>
+            <li><a href="">Current Takeovers and Delisting - January 2026</a></li>
+            <li><a href="">Latest Intraday leverages - MIS & CO</a></li>
+          </ol>
+          
+          
+        </div>
+      </div>
+    </section>
      );
 }
 
