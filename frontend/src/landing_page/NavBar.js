e9886469e6638190f0459183492731cde0f25a70
Rahul <rd2432007@gmail.com>
2026-03-23 21:15:22 +0530

    code updated

--- frontend/src/landing_page/NavBar.js
+++ frontend/src/landing_page/NavBar.js
@@ -1,29 +1,43 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-      <nav class="navbar navbar-expand-lg border bottom" style={{backgroundColor:"#FFF"}}
-      >
-        <div class="container p-2">
-          <Link class="navbar-brand" to="/">
-            <img src="media\images\logo.svg" alt="logo" style={{width:"25%"}}/>
-          </Link>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <form class="d-flex" role="search">
-                <ul class="navbar-nav mb-lg-0">
+    <nav
+      class="navbar navbar-expand-lg border bottom"
+      style={{ backgroundColor: "#FFF" }}
+    >
+      <div class="container p-2">
+        <Link class="navbar-brand" to="/">
+          <img
+            src="media\images\logo.svg"
+            alt="logo"
+            style={{ width: "25%" }}
+          />
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <form class="d-flex" role="search">
+            <ul class="navbar-nav mb-lg-0">
+              <li class="nav-item d-flex justify-content-center w-100">
+                <img
+                  src="media/images/logo (1).png"
+                  style={{ width: "30%", cursor: "pointer" }}
+                  onClick={() =>
+                    window.open("http://localhost:3001/", "_blank")
+                  }
+                />
+              </li>
               <li class="nav-item">
                 <Link class="nav-link active" aria-current="page" to="/signup">
                   Signup
@@ -31,33 +45,29 @@ function NavBar() {
               </li>
               <li class="nav-item">
                 <Link class="nav-link active" to="/about">
-                 About
+                  About
                 </Link>
               </li>
               <li class="nav-item">
                 <Link class="nav-link active" to="/product">
-                 Product
+                  Product
                 </Link>
               </li>
               <li class="nav-item">
                 <Link class="nav-link active" to="/pricing">
-                 Pricing
+                  Pricing
                 </Link>
               </li>
               <li class="nav-item">
-                <Link class="nav-link active"to="/support">
-                 Support
+                <Link class="nav-link active" to="/support">
+                  Support
                 </Link>
               </li>
-                
-              
             </ul>
-
-            </form>
-          </div>
+          </form>
         </div>
-      </nav>
-
+      </div>
+    </nav>
   );
 }
 
